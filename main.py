ed02fc6096a3fa186a7c4dfb2ff92a31f4f4b627
kramoris <139485903+kramoris@users.noreply.github.com>
2025-04-22 10:52:02 +0200

    Add Task class and test with in-memory task list

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+class Task:
+    def __init__(self, title):
+        self.title = title
+        self.completed = False
+
+    def __str__(self):
+        status = "[x]" if self.completed else "[ ]"
+        return f"{status} {self.title}"
+
+tasks = []
+
 def main():
-    print("Welcome to CLI Task Tracker!")
+    task = Task("Finish building CLI Task Tracker")
+    tasks.append(task)
+
+    print("Tasks:")
+    for t in tasks:
+        print(t)
 
 if __name__ == "__main__":
     main()
